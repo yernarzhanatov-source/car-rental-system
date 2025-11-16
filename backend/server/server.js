86b134acccf965a3d68375477efcf75248146524
yernarzhanatov-source <yernarzhanatov@gmail.com>
2025-11-16 17:07:36 +0500

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+const userRoutes = require('./routes/users');
+const carRoutes = require('./routes/cars');
+const bookingRoutes = require('./routes/bookings');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/users', userRoutes);
+app.use('/cars', carRoutes);
+app.use('/bookings', bookingRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: "Car Rental API is running" });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
